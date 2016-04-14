465104f6cfce61c1f5b116a8ba1461be6e0b1e94
Cristian Gutiérrez <yayobyte@gmail.com>
2016-04-13 19:14:03 -0500

    Finished pill class

--- src/ejpilas/Pill.java
+++ src/ejpilas/Pill.java
@@ -20,5 +20,44 @@ public class Pill {
         this.pill = new String [size];
     }
     
+    public boolean isEmpty (){
+        return (this.pointer == 0)? true:  false;
+    }
     
+    public boolean isFull () {
+        return (this.pointer == this.size)? true : false;
+    }
+    
+    public boolean push (String data) {
+        if (this.isFull()){
+            return false;
+        }else {
+            pill[this.pointer] = data;
+            this.pointer ++;
+            return true;
+        }
+    }
+    
+    public boolean pop (){
+        if (this.isEmpty()){
+            return false;
+        }else{
+            this.pill[this.pointer] = null;
+            this.pointer --;
+            return true;
+        }
+    }
+    
+    public String print () {
+        String data="";
+        if (this.isEmpty()){
+            data =  "Pila vacía";
+        }else{
+            for (int i = 0; i<this.pointer; i++){
+                data = this.pill[i] + "\n";
+            }
+        }
+        return data;
+            
+    }
 }
